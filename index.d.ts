b148b3d84d48f1da1708ca8eb8b042f819c859a3
Miguel Savignano <m.savignano@stuart.com>
2022-12-22 00:04:14 +0100

    build

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { OpenTelemetryModule, OpenTelemetryModuleConfig } from '@metinseylan/nes
 export { Span, TraceService } from '@metinseylan/nestjs-opentelemetry';
 import { ExporterConfig } from '@opentelemetry/exporter-jaeger';
 export declare class OpenTelemetrySetupModule extends OpenTelemetryModule implements NestModule {
-    static fromRoot(configuration?: Partial<OpenTelemetryModuleConfig>): Promise<DynamicModule>;
+    static forRoot(configuration?: Partial<OpenTelemetryModuleConfig>): Promise<DynamicModule>;
     configure(consumer: MiddlewareConsumer): void;
     static traceAutoInjectors(): Array<any>;
     static jaegerExporterOptions(): ExporterConfig;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var OpenTelemetrySetupModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.OpenTelemetrySetupModule = exports.TraceService = exports.Span = void 0;
 const common_1 = require("@nestjs/common");
@@ -15,17 +16,17 @@ Object.defineProperty(exports, "Span", { enumerable: true, get: function () { re
 Object.defineProperty(exports, "TraceService", { enumerable: true, get: function () { return nestjs_opentelemetry_2.TraceService; } });
 const exporter_jaeger_1 = require("@opentelemetry/exporter-jaeger");
 const sdk_trace_base_1 = require("@opentelemetry/sdk-trace-base");
-let OpenTelemetrySetupModule = class OpenTelemetrySetupModule extends nestjs_opentelemetry_1.OpenTelemetryModule {
-    static async fromRoot(configuration = {}) {
-        const fromRoot = await super.forRoot(Object.assign(Object.assign({}, configuration), { traceAutoInjectors: this.traceAutoInjectors(), spanProcessor: new sdk_trace_base_1.SimpleSpanProcessor(new exporter_jaeger_1.JaegerExporter(this.jaegerExporterOptions())) }));
-        return Object.assign({}, fromRoot);
+let OpenTelemetrySetupModule = OpenTelemetrySetupModule_1 = class OpenTelemetrySetupModule extends nestjs_opentelemetry_1.OpenTelemetryModule {
+    static async forRoot(configuration = {}) {
+        const forRoot = await super.forRoot(Object.assign(Object.assign({}, configuration), { traceAutoInjectors: this.traceAutoInjectors(), spanProcessor: new sdk_trace_base_1.SimpleSpanProcessor(new exporter_jaeger_1.JaegerExporter(this.jaegerExporterOptions())) }));
+        return Object.assign(Object.assign({}, forRoot), { module: OpenTelemetrySetupModule_1 });
     }
     configure(consumer) {
         consumer
             .apply((req, res, next) => {
             var _a;
             const spanContext = (_a = api_1.trace.getSpan(api_1.context.active())) === null || _a === void 0 ? void 0 : _a.spanContext();
-            res.set('x-traceid', spanContext === null || spanContext === void 0 ? void 0 : spanContext.traceId);
+            res.set('x-traceid', (spanContext === null || spanContext === void 0 ? void 0 : spanContext.traceId) || 'unknown');
             next();
         })
             .forRoutes('*');
@@ -44,7 +45,7 @@ let OpenTelemetrySetupModule = class OpenTelemetrySetupModule extends nestjs_ope
         return {};
     }
 };
-OpenTelemetrySetupModule = __decorate([
+OpenTelemetrySetupModule = OpenTelemetrySetupModule_1 = __decorate([
     (0, common_1.Module)({})
 ], OpenTelemetrySetupModule);
 exports.OpenTelemetrySetupModule = OpenTelemetrySetupModule;
